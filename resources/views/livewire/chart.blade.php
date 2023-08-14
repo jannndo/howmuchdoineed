b1a96e99339a294aa79c2212efa0e0a82bb12f0f
jannndo <jan.klatik.code@gmail.com>
2023-08-14 23:58:16 +0200

    minor style changes for the chart div

--- resources/views/livewire/chart.blade.php
+++ resources/views/livewire/chart.blade.php
@@ -3,8 +3,18 @@
         initChart: window.ChartComponent().initChart 
     }" 
     x-init="initChart()" 
-    id="chartContainer">
+    id="chartContainer"
+    class="flex justify-center items-center bg-gray-100">
 
-    <div style="width: 800px;"><canvas id="data"></canvas></div>
-    <pre x-text="JSON.stringify(chartData, null, 2)"></pre>
+    <div class="bg-white p-8 rounded-xl shadow-lg w-full md:max-w-2xl">
+        <h2 class="text-2xl font-bold mb-6 text-center">Your Chart Title</h2>
+        <div class="flex justify-center w-full">
+            <!-- Set canvas to occupy full width of its parent -->
+            <canvas id="data" class="w-full"></canvas>
+        </div>
+    </div>
+
+    <!-- for debugging purposes -->
+    <!-- <pre x-text="JSON.stringify(chartData, null, 2)" class="mt-4 text-xs"></pre> -->
+    
 </div>
