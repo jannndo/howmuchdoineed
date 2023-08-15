ad3d59f646a5a4b49787ede608981e3344c05546
jannndo <jan.klatik.code@gmail.com>
2023-08-15 22:54:15 +0200

    Fix the automatic chart container width

--- resources/views/livewire/chart.blade.php
+++ resources/views/livewire/chart.blade.php
@@ -4,11 +4,11 @@
     }" 
     x-init="initChart()" 
     id="chartContainer"
-    class="flex justify-center items-center bg-gray-100">
+    class="mt-4 flex justify-center items-center bg-gray-100">
 
     @if(!empty($chartData))
 
-        <div class="bg-white p-8 rounded-xl shadow-lg w-full md:max-w-2xl">
+        <div class="bg-white p-8 rounded-xl shadow-lg w-full ">
             <h2 class="text-2xl font-bold mb-6 text-center">Your Chart Title</h2>
             <div class="flex justify-center w-full">
                 <!-- Set canvas to occupy full width of its parent -->
